b7ca439b0a77b258ecca5db3e07f26413e3d046b
Reto Schläpfer <reto.schlaepfer.rs@gmail.com>
2015-02-04 16:36:57 +0100

    firefox bug

--- src/parseOptions.js
+++ src/parseOptions.js
@@ -1,11 +1,11 @@
 function parseOptions() {
     var devServerOptions = document.getElementById("dev-server");
-    if (devServerOptions) {
+    if (devServerOptions && devServerOptions.innerText) {
         return JSON.parse(devServerOptions.innerText);
     }
 
     var otherOptions = document.getElementById("raml-doc");
-    if (otherOptions) {
+    if (otherOptions && otherOptions.innerText) {
         return JSON.parse(otherOptions.innerText);
     }
 
